e163f1242ca90fa96184427b9ab4627bf9640a55
unknown <borgen_12@hotmail.com>
2021-08-17 12:53:51 -0700

    layout changes

--- examples/index.js
+++ examples/index.js
@@ -1,25 +1,35 @@
 'use strict';
-const escpos = require('..');
-
-const device  = new escpos.USB(0x0416, 0x5011);
+const { default: axios } = require('axios');
+const escpos = require('escpos');
+escpos.USB = require('escpos-usb')
+const device  = new escpos.USB(0x04b8, 0x0202);
 // const device  = new escpos.RawBT();
 // const device  = new escpos.Network('localhost');
 // const device  = new escpos.Serial('/dev/usb/lp0');
 const printer = new escpos.Printer(device);
+function getAwaitingOrders() {
+  try {
+    const order = await (await axios.get())
+    device.open(function(err){
 
-device.open(function(err){
+      printer
+      .font('b')
+      .align('ct')
+      .style('bu')
+      .size(.01, .01)
+      .text(order.name)
+      .text(order.id)
+      .barcode('1234567', 'EAN8')
+      .qrimage('https://github.com/song940/node-escpos', function(err){
+        this.cut();
+        this.close();
+      });
+    
+    });
 
-  printer
-  .font('a')
-  .align('ct')
-  .style('bu')
-  .size(1, 1)
-  .text('The quick brown fox jumps over the lazy dog')
-  .text('敏捷的棕色狐狸跳过懒狗')
-  .barcode('1234567', 'EAN8')
-  .qrimage('https://github.com/song940/node-escpos', function(err){
-    this.cut();
-    this.close();
-  });
+  }
+  catch(e) {
+    console.log('NOGO', e);
+  }
+}
 
-});
