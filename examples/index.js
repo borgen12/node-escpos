b2c2ed52b4ce997efa87912b9e1d16175dd3fe89
unknown <borgen_12@hotmail.com>
2021-08-18 10:16:49 -0700

    activste server

--- examples/index.js
+++ examples/index.js
@@ -7,19 +7,21 @@ const device  = new escpos.USB(0x04b8, 0x0202);
 // const device  = new escpos.Network('localhost');
 // const device  = new escpos.Serial('/dev/usb/lp0');
 const printer = new escpos.Printer(device);
+getAwaitingOrders();
 function getAwaitingOrders() {
   try {
-    const order = await (await axios.get())
+    //const order = await (await axios.get())
     device.open(function(err){
 
       printer
       .font('b')
       .align('ct')
-      .style('bu')
+      .style('b')
       .size(.01, .01)
-      .text(order.name)
-      .text(order.id)
+      .text('yo')
+      .text('order.id')
       .barcode('1234567', 'EAN8')
+      
       .qrimage('https://github.com/song940/node-escpos', function(err){
         this.cut();
         this.close();
--- examples/server.js
+++ examples/server.js
@@ -1,10 +1,40 @@
-const escpos = require('..');
-
-const device = new escpos.USB();
+const escpos = require('escpos');
+escpos.USB = require('escpos-usb')
+escpos.Server = require('../packages/server/server')
+const device = new escpos.USB(0x04b8, 0x0202);
 const server = new escpos.Server(device);
+const printer = new escpos.Printer(device);
 
-device.open(() => {
+
   server.listen(6000, err => {
     console.log('Your printer is running at', server.address().port);
-  });
-});
+    setInterval(() => getAwaitingOrders(), 5000)
+
+    function getAwaitingOrders() {
+      console.log('AGAIN');
+      try {
+        //const order = await (await axios.get())
+        device.open(function (err) {
+
+          printer
+            .font('b')
+            .align('ct')
+            .style('b')
+            .size(.01, .01)
+            .text('yo')
+            .text('order.id')
+
+            .qrimage('https://github.com/song940/node-escpos', function (err) {
+              this.cut();
+              this.close();
+            });
+
+        });
+
+      }
+      catch (e) {
+        console.log('NOGO', e);
+      }
+    }
+  })
+
